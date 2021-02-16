bb06020f15102090e0dbde41a293db44677fe670
keitasumiya <ksumiya0318@gmail.com>
2021-02-17 00:42:59 +0900

    add KlakSpout sender

---
+++ 2019-4-14/spout/klakSpoutSender/Assets/Scripts/textPositionChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textPositionChanger : MonoBehaviour
+{
+    public GameObject textGO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void buttonUp()
+    {
+        textGO.transform.Translate(0, 1, 0);
+    }
+
+    public void buttonDown()
+    {
+        textGO.transform.Translate(0, -1, 0);
+    }
+
+}

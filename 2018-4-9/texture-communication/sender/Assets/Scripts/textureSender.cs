591289298da32096f5c1bf016712aa8348cd1412
Keita Sumiya <ksumiya0318@gmail.com>
2021-02-16 22:44:13 +0900

    textureをポインタ経由で取得する練習のため. 未理解, 未完成.

---
+++ 2018-4-9/texture-communication/sender/Assets/Scripts/textureSender.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class textureSender : MonoBehaviour
+{
+    //IntPtr ptr = GetNativeTexturePtr();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

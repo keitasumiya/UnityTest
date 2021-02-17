1df81609028d4081a744862994505525c8e42ca2
keitasumiya <ksumiya0318@gmail.com>
2021-02-17 11:15:19 +0900

    made a transparent function. But button like functions are not available now.

---
+++ 2019-4-14/transparentBG/transparentBG/Assets/Scripts/ActivateButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateButton : MonoBehaviour
+{
+    public GameObject GO;
+    public Toggle toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate()
+    {
+        GO.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        GO.SetActive(false);
+    }
+
+    public void SetActive()
+    {
+        Debug.Log(toggle.isOn);
+        if (toggle.isOn)
+        {
+            GO.SetActive(true);
+        }
+        else
+        {
+            GO.SetActive(false);
+        }
+    }
+
+}

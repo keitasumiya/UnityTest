74ec201c762c3f00ca5788c6f9bedf87ff588e4d
keitasumiya <ksumiya0318@gmail.com>
2021-02-17 12:49:27 +0900

    checked a key press down is available while background is transparent!!

---
+++ 2019-4-14/transparentBG/transparentBG/Assets/Scripts/KeyActivator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyActivator : MonoBehaviour
+{
+    public GameObject GO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GO.activeSelf)
+            {
+                GO.SetActive(false);
+            }
+            else
+            {
+                GO.SetActive(true);
+            }
+        }
+        
+    }
+}
